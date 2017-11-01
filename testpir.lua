3fa54f01b0874489758a967e12520d3db5aa87e6
Rudi Niemeijer <rudiniemeijer@gmail.com>
2017-11-01 15:57:12 +0100

    Update testpir.lua

--- testpir.lua
+++ testpir.lua
@@ -6,13 +6,14 @@ ledpin, pirsensorpin = 4, 2
 gpio.mode(pirsensorpin, gpio.INT, gpio.PULLUP)
 gpio.mode(ledpin, gpio.OUTPUT)
 
-function movement()
-  print("PIR sensor detected movement")
-  gpio.write(ledpin, gpio.LOW)                    -- gpio.LOW is led ON
-  tmr.alarm(0, 2000, tmr.ALARM_SINGLE, function() -- led off after 2 seconden
-    gpio.write(ledpin, gpio.HIGH)                 -- gpio.HIGH is led OFF
+function beweging()
+  print(tmr.time() .. "PIR sensor detecteerde beweging")
+  gpio.write(ledpin, gpio.LOW)                      -- gpio.LOW is led AAN
+  tmr.alarm(0, 2000, tmr.ALARM_SINGLE,
+  function()                                        -- led uit na 2 seconden
+    gpio.write(ledpin, gpio.HIGH)                   -- gpio.HIGH is led UIT
   end)
 end
 
--- Call the movement function if the signal goes from low to high ("up")
-gpio.trig(pirsensorpin, "up", movement) -- Reference to the movement function
+-- Roep de beweging functie aan als het signaal van laag naar hoog gaat ('up')
+gpio.trig(pirsensorpin, "up", beweging)             -- Referentie naar de beweging functie
