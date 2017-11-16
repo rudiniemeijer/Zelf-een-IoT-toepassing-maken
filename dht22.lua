2d1fffc67e23e9f8298b806766adeb6aafa8e27a
Rudi Niemeijer <rudiniemeijer@gmail.com>
2017-11-16 09:10:31 +0100

    Create dht22.lua

---
+++ dht22.lua
@@ -0,0 +1,53 @@
+-- dht22.lua
+-- Copyright (c) 2017 Rudi Niemeijer
+-- MIT license
+
+ledpin, dhtsensorpin = 4, 2
+gpio.mode(ledpin, gpio.OUTPUT)
+
+-- Change this
+IFTTT_APPLET = "node_dht_store"
+IFTTT_API_KEY = "pWixgavdPXBK6n6vbjWr3xJZoc7V_hDY8NdNIs8Wq68"
+THS_API_KEY = "QQKSHKKOVC23RFHW"
+INTERVAL = 30000
+HUM_MAX = 78
+
+-- Do not change this
+IFTTT_URL = "http://maker.ifttt.com/trigger/"..IFTTT_APPLET.."/with/key/"..IFTTT_API_KEY
+THS_URL = "http://api.thingspeak.com/update?api_key="..THS_API_KEY
+
+function readDHT22()
+  dht = require("dht")
+  local status, temperature, humidity = dht.read(dhtsensorpin)
+    if status ~= dht.OK then
+      print("DHT Time out or Checksum error")
+      temperature = nil
+      humidity = nil
+    end
+    dht = nil
+    package.loaded["dht"] = nil
+    return temperature, humidity
+end
+
+tmr.alarm(5, INTERVAL, tmr.ALARM_AUTO, function() 
+  gpio.write(ledpin, gpio.LOW)                    -- gpio.LOW is led ON
+  t, h = readDHT22()
+  if t ~= nil then
+    if wifi.sta.status() == 5 then -- Valid IP address?
+      tmr.alarm(0, INTERVAL / 100, tmr.ALARM_SINGLE, function() -- Led off after a short while
+        gpio.write(ledpin, gpio.HIGH)                 -- gpio.HIGH is led OFF
+      end)
+      http.get(THS_URL.."&field1="..t.."&field2="..h)
+      print("Sent temperature "..t.." deg C and humidity "..h.."% to ThingSpeak")      
+      if h > HUM_MAX then
+        http.get(IFTTT_URL.."?value1="..t.."&value2="..h)
+        print("Humidity is higher than "..HUM_MAX.." so reached out to IFTTT as well")
+      end
+    else
+      tmr.alarm(0, INTERVAL / 2, tmr.ALARM_SINGLE, function() -- Led off after longer while
+        gpio.write(ledpin, gpio.HIGH)                 -- gpio.HIGH is led OFF
+      end)
+      print("No IP address. Use wifi.sta.config() first")
+    end
+  end 
+end)
