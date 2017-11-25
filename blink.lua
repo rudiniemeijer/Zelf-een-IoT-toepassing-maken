08a2befbb819b030704bb802b7313da3a416d152
Rudi Niemeijer <rudiniemeijer@gmail.com>
2017-11-25 07:54:04 +0100

    Update blink.lua

--- blink.lua
+++ blink.lua
@@ -3,6 +3,8 @@
 -- Copyright (c) 2017 Rudi Niemeijer
 -- MIT license
 
+-- Required modules in NodeMCU firmware: gpio
+
 wachttijd = 1000  -- ms tussen AAN en UIT
 ledpin = 0        -- D0 (er is ook een led op D4)
 ledaan = 0        -- status van de led
